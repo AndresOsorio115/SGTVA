32bbfdde83474f11cacfbb3618e29775c8ba496e
Andres Osorio <andresangel115@hotmail.com>
2016-05-09 20:15:45 -0500

    Documentación del Login Principal
    Se documenta la clase junto con sus funcionalidades.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,21 +3,34 @@
 if ( ! defined('BASEPATH')){
  exit('No direct script access allowed');
 }
- 
-class Login extends CI_Controller {
- 
-	 
 
+/**
+ * Esta clase es la encargada de realizar el login del software,
+ * funciona validando los campos de usuario y contrasenia,
+ * estos datos fueron entregados al cliente previamente.
+ */
+class Login extends CI_Controller {
+
+	/**
+	 * Constructor de la clase
+	 */
 	 function __construct() {
 	   parent::__construct();
 	 }
 	 
+	 /**
+	  * Metodo index que carga la vista del login
+	  */
 	 function index() {
 	   
 	   $this->load->helper(array('form'));
 	   $this->load->view('login_view');
 	 }
-
+	
+	/**
+	 * Este es el metodo encargado de realizar las validaciones de usuario y contrasenia
+	 * para realizar un login exitoso.
+	 */
 	 public function login(){
 
 		if($_POST) {	
